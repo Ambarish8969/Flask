80e9b073f0125b375f968c168c9bcb91a387e9a3
Ambarish Bhagawati <ambarish8653@gmail.com>
2023-01-17 18:27:23 +0530

    added

--- f2.py
+++ f2.py
@@ -1,39 +1,71 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+
+# Init App
 app=Flask(__name__)
 
+# Database Configuration
 app.config["SQLALCHEMY_DATABASE_URI"]= "mysql://root:@localhost/clients2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Mail Configuration
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'ambarish.bhagawati8653@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+# Initalize DB
 db = SQLAlchemy(app)
+mail = Mail(app)
 db.init_app(app)
 
 class Users2(db.Model):
     Username=db.Column(db.String,primary_key=True)
     Password=db.Column(db.String)
 
-    # def __init__(self,Username,Password):
-    #     self.Username=Username
-    #     self.Password=Password 
+    def __init__(self,Username,Password):
+        self.Username=Username
+        self.Password=Password 
+
+@app.route('/mail')
+def email():
+    msg = Message( 'Hello Message', sender='ambarish.bhagawati8653@gmail.com', recipients=['ambarishbhagawati8969@gmail.com'])
+    mail.send(msg)
 
 @app.route('/showall')
 def show_all():
    return render_template('Show_all.html', users2 = Users2.query.all() )
 
-@app.route('/showall/<string:user>')
+@app.route('/api/<string:user>')
 def show_employee(user):
     user=Users2.query.filter_by(Username=user).first()
-    return [user.Username,user.Password]
+    data={
+        'Username':user.Username,
+        'Password':user.Password
+    }
+    return jsonify(data)
+
+@app.route('/api')
+def show_employees():
+    users=Users2.query.all()
+    data=[{
+            "Username": user.Username,
+            "Password": user.Password,
+        } for user in users]
+    return jsonify(data)
 
 @app.route('/createuser',methods=['GET','POST'])
 def create():
     if(request.method=='POST'):
         username2=request.form.get('username2')
         password2=request.form.get('password2')
-    
         entry=Users2(Username=username2,Password=password2)
         db.session.add(entry)
         db.session.commit()
-    return render_template('Create.html')
+    return render_template('Create.html')      
 
 @app.route('/delete/<string:username>',methods=['GET','POST','DELETE'])
 def delete(username):
@@ -42,7 +74,7 @@ def delete(username):
         db.session.delete(entry)
         db.session.commit()
         return f"<p>User {username} Deleted.</p> <a href='/showall'>Go to Showall Users</a>"
-    return render_template('Delete.html')
+    return render_template('Delete.html',username=username)
 
 @app.route('/edit/<string:username>',methods=['GET','POST'])
 def edituser(username):
@@ -64,4 +96,5 @@ def Home():
 def Info(username):
     return render_template('Info.html',username1=username)
 
+# Run Server
 app.run(debug=True)
